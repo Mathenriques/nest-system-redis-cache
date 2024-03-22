4b39076d600dbeb40469bb3589671487e1645cc4
Mathenriques <math.marqui@gmail.com>
2024-03-22 19:07:09 -0300

    Add cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './config/prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 
 @Module({
   imports: [
+    CacheModule.register({isGlobal: true}),
     PrismaModule,
     UsersModule
+
   ],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/config/prisma/prisma.service";
 import { CreateUserDto } from "./users.dto";
+import { Cache } from "cache-manager";
 
 @Injectable()
 export class UsersServices {
 
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    @Inject('CACHE_MANAGER') private cacheManager: Cache,
+    private readonly prismaService: PrismaService
+  ) {}
 
 
   public async getAllUsers() {
-    return await this.prismaService.user.findMany();
+    const cachedUsers = await this.cacheManager.get('users');
+
+    if (cachedUsers) {
+      console.log("Achado no Cache!", cachedUsers);
+      return cachedUsers;
+    }
+
+    console.log('Busca no Banco de dados!')
+    const usersDb = await this.prismaService.user.findMany();
+
+    await this.cacheManager.set('users', usersDb, 10000)
+
+    return usersDb 
+
   }
   
   public async createUser(data: CreateUserDto) {
