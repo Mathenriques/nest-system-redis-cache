48b70de8a366540fa0ca2581c9646f52a595f6f9
Mathenriques <math.marqui@gmail.com>
2024-03-22 19:58:27 -0300

    Add redis no system

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,30 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './config/prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
 import { redisStore } from 'cache-manager-redis-yet';
 
 
 @Module({
   imports: [
-    CacheModule.register({ 
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true
+    }),
+    CacheModule.registerAsync({ 
       isGlobal: true, 
-      ttl: 30 * 1000,
-      store: redisStore
+      useFactory: async (config) => {
+        const store = await redisStore({
+          ttl: 30 * 1000,
+          socket: {
+            host: config.get("redis.host"),
+            port: config.get("redis.port")
+          }
+        });
+        return { store }
+      },
+      inject: [ConfigService]
     }),
     PrismaModule,
     UsersModule
